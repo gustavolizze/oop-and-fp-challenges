8fd98257e2fb2b42555fcf49897bbd1b4347bee6
gustavolizze <gustavolizze@hotmail.com>
2021-07-05 03:56:14 -0300

    feat: added http endpoint

--- challenges/backend/src/main.ts
+++ challenges/backend/src/main.ts
@@ -3,9 +3,33 @@ import sourceMapSupport from 'source-map-support';
 import 'reflect-metadata';
 import 'make-promises-safe';
 
-export function main(): void {
+import fastify from 'fastify';
+
+// Plugins
+import helmet from 'fastify-helmet';
+import cors from 'fastify-cors';
+import compress from 'fastify-compress';
+
+//Use cases
+import { GetProductsUseCase } from './use-cases';
+
+export async function main() {
 	// Dependencies
 	sourceMapSupport.install({
 		environment: 'node',
+	});
+
+	const app = fastify();
+
+	await app.register(helmet);
+	await app.register(cors);
+	await app.register(compress, {
+		encodings: ['gzip', 'deflate'],
+	});
+
+	app.get('/', async () => new GetProductsUseCase().execute());
+
+	app.listen(8080, '0.0.0.0').then(() => {
+		console.log(app.printRoutes());
 	});
 }
