e5bbf4b3d635b2ea4d5c3951c5ac62d95be9d704
gustavolizze <gustavolizze@hotmail.com>
2021-07-05 03:35:12 -0300

    feat: added infra

---
+++ challenges/backend/src/infra/database/in-memory/index.ts
@@ -0,0 +1,7 @@
+import { Product } from '../../../domain/models';
+
+export const Products: Product[] = [
+	new Product({ name: 'Tenis nike', price: 550, reviews: 5 }),
+	new Product({ name: 'Tenis adidas', price: 1200, reviews: 4.5 }),
+	new Product({ name: 'Moleton Fifa', price: 120.3, reviews: 2.5 }),
+];
